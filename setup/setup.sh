72185944cb4bfcfc9364eefa9a962306667d2d49
Andrew Au <andrew.au@cbc.ca>
2017-03-10 19:26:00 -0500

    added necessary packages to be installed by apt-get

---
+++ setup/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Apache2
+sudo apt-get -y install apache2
+sudo apt-get -y install libapache2-mod-wsgi-py3
+
+# Python
+sudo apt-get -y install python3.6
+sudo apt-get -y install python3.6-dev
+sudo apt-get -y install postgresql-9.6
+sudo apt-get -y install python-pip
+
+sudo pip install virtualenv
