b3cd6467b9769a0c76a91bfb05bf3852acb9d04c
Andrew Au <andrew.au@cbc.ca>
2017-03-10 19:26:40 -0500

    changed grouping of installations

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,7 +9,9 @@ sudo apt-get -y install libapache2-mod-wsgi-py3
 # Python
 sudo apt-get -y install python3.6
 sudo apt-get -y install python3.6-dev
-sudo apt-get -y install postgresql-9.6
 sudo apt-get -y install python-pip
+
+# PostgreSQL
+sudo apt-get -y install postgresql-9.6
 
 sudo pip install virtualenv
